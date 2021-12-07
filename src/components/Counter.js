125bc72f1c2745fe8495112bb0e64f4cfc9cab19
Chris DeCleene <chris_decleene@ymail.com>
2021-12-07 16:30:49 -0700

    Initial Commit

---
+++ src/components/Counter.js
@@ -0,0 +1,44 @@
+// import { Component } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./Counter.module.css";
+import { counterActions } from "../store/counter";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector(state => state.counter.showCounter);
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
+
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
+  };
+
+  const increaseHandler = () => {
+    dispatch(counterActions.increase({ amount: 10 }));
+  };
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter())
+  };
+
+  const counterElement = (<main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
+        <button onClick={decrementHandler} disabled={counter === 0}>
+          Decrement
+        </button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>)
+
+  return isAuthenticated && counterElement;
+};
+
+export default Counter;
